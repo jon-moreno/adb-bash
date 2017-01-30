b25f368c75edd1c9957f5da81bce3d87eec2cebc
Jonathan Moreno <jonathan.moreno324@gmail.com>
2017-01-30 14:50:31 -0500

    Changed name of screenshot to match Android and use date.

--- adb-scrot.sh
+++ adb-scrot.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 #Automate the process of taking android screencaps with sequential filenames
-#Credit to whomever wrote this
-#http://blog.shvetsov.com/2013/02/grab-android-screenshot-to-computer-via.html
+#Credit to whomever wrote this:
+##http://blog.shvetsov.com/2013/02/grab-android-screenshot-to-computer-via.html
 
 #Create an option for method choice
 #Ensure valid file is created
 
-#create numbering scheme and way to sequence
-#probably just going to use the date and match android scheme tbh
-adb shell screencap -p | sed 's/\r$//' > screen1.png
+#Matches Android format scheme
+##Screenshot_YYYY-mm-DD-HH-MM-SS.png
+frmt=date +"%F-%H-%M-%S"
+adb shell screencap -p | sed 's/\r$//' > Screenshot_$frmt.png
+#adb -s SERIALNUM shell screencap -p | sed 's/\r$//' > screen1.png
+#RIGHT_NOW=$(date +"%x %r %Z")
 
 #Since the Pixel C produces invalid images with the previous command
 #adb shell screencap /sdcard/Pictures/Screenshots/screen1.png
 #have to make it the file from before
 #Guess we'll hardcode the save location to keep things organized
 #adb pull /sdcard/Pictures/Screenshots/screen1.png ~/Pictures/Android Screenshots/
+
+#Alternative idea: screenshot on device and pull all the newest screenshots
 
 exit 0
