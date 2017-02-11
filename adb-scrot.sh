c0f538a80c47697b851d96379dce862d572fb5cd
jon-moreno <jonathan.moreno324@gmail.com>
2017-02-10 23:06:57 -0500

    Beginnings for choice of method to scrot with.

--- adb-scrot.sh
+++ adb-scrot.sh
@@ -9,26 +9,29 @@
 #Matches Android format scheme
 ##Screenshot_YYYY-mm-DD-HH-MM-SS.png
 now=$(date +"%F-%H-%M-%S")
-filename="Screenshot_"$now".png"
+generated_filename="Screenshot_"$now".png"
 directory=""
-serial=$1
-#adb shell screencap -p | sed 's/\r$//' > Screenshot_$now.png
-#Works with Linux + Mac
-adb -s $serial shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $filename
-#adb -s SERIALNUM shell screencap -p | sed 's/\r$//' > screen1.png
-#RIGHT_NOW=$(date +"%x %r %Z")
 
 #The Pixel C produces invalid images with the previous command
-#Hope space doesn't become an issue
-#adb shell screencap /sdcard/Pictures/Screenshots/$filename.png
-#have to make it the file from before
-#Guess we'll hardcode the save location to keep things organized
-#adb pull /sdcard/Pictures/Screenshots/$filename ~/Pictures/Android Screenshots/$filename
+if [ "$#" eq 1 ]; then
+	
+	serial=$1
+	#Works with Linux + Mac
+	adb -s $serial shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $generated_filename
+
+elif [ "$#" eq 2 ]; then
+
+	serial=$2
+	adb shell screencap /sdcard/Pictures/Screenshots/$generated_filename
+
+	#saved_filename=$(adb shell pm ls)
+	#get last file in list, should be latest screenshot
+	adb pull /sdcard/Pictures/Screenshots/$saved_filename ~/Pictures/Android Screenshots/$saved_filename
+else
+
+	echo "Invalid number of parameters"
+fi
 
 #Alternative idea: screenshot on device and pull all the newest screenshots
-
-#So I think I'll have to officially declare the Pixel C an edge case
-#I'll take in a parameter for when I have to do scrot on Pixel C
-#Otherwise do original script
 
 exit 0
